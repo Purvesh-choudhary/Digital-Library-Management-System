f4a171696bb985598e99458b508d7973a32488a5
Purveh Choudhary <98455640+Purvesh-choudhary@users.noreply.github.com>
2025-03-05 19:03:13 +0530

    adding files

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; <?php echo date('Y');?> Digital Library Management System |<a href="https://gamedevpurvesh.blogspot.com/" target="_blank"  > Designed by : Purvesh Choudhary & Abhishek Jaiswal </a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; <?php echo date('Y');?> Digital Library Management System | Designed by : Purvesh Choudhary & Abhishek Jaiswal</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
